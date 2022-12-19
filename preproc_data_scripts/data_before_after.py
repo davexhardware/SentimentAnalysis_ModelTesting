4027e37f3d4d981ab8b51a170d6ab0111e22fc6c
davexhardware <davexsoia@gmail.com>
2022-12-19 18:12:48 +0100

    1.1. Updated requirements

--- preproc_data_scripts/data_before_after.py
+++ preproc_data_scripts/data_before_after.py
@@ -2,7 +2,7 @@ import pandas as pd
 import random
 
 dataB=pd.read_csv('../datafile/IMDB Dataset.csv')
-dataA=pd.read_csv('datafile/preproc_data.csv')
+dataA=pd.read_csv('../datafile/preproc_data_def.csv')
 for j in range(0,10):
     i=random.randint(0,dataA.shape[0])
     print('Review',i, '\n--------------------------- Raw data: --------------------------------')
